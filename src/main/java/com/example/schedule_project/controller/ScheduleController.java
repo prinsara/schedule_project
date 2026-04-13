a44886c91ce40553a6b1bd787ef2dbe9ef499363
사라 <prinsaraouo@gmail.com>
2026-04-13 18:00:44 +0900

    refactor(schedule): 조회/검증 공통 메서드 분리

--- src/main/java/com/example/schedule_project/controller/ScheduleController.java
+++ src/main/java/com/example/schedule_project/controller/ScheduleController.java
@@ -11,50 +11,55 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/schedules")
 public class ScheduleController {
 
     private final ScheduleService scheduleService;
 
 
     //저장
-    @PostMapping("/schedules")
+    @PostMapping
     //보여줘야 돼 뭘 보여줘야 돼?!?!?!?!! 응답 메서드 만들기
     //요청이 온 거를
     public ResponseEntity<CreateScheduleResponse> createSchedule(@RequestBody CreateScheduleRequest request) {
         //서비스에서 만든 메서드 활용 사용자에게 보여주기
-        CreateScheduleResponse createResult = scheduleService.addSchedule(request);
-        return ResponseEntity.status(HttpStatus.CREATED).body(createResult);
+       return ResponseEntity.status(HttpStatus.CREATED).body(scheduleService.addSchedule(request));
     }
 
-    //전체조회
-    @GetMapping("/schedules")
-    public ResponseEntity<List<GetScheduleResponse>> getAllSchedule(
-            //URL에 붙어서 온 요청 파라미터 받기
-            //GET/schedules?name=사라 이런 식으로 적었을 경우에
-            //파람을 붙이면 해당 URL에서 name=사라를 꺼내서 메서드 안으로 넣어준다.
+    /*
 
-            //required = false -> 파라미터가 꼭 없어도 된다. 없으면 null
-            //만약 true 였으면 무조건 있어야 하며, 없을 경우 에러남
-            //URL에 name 요청값이 있으면 문자열로 받아서 name 변수에 넣고 없어도 에러 내지 말기
+    메서드 밖으로 주석 빼버렸어용! 그
+    URL에 붙어서 온 요청 파라미터 받기
+    GET/schedules?name=사라 이런 식으로 적었을 경우에
+    파람을 붙이면 해당 URL에서 name=사라를 꺼내서 메서드 안으로 넣어준다.
+
+    required = false -> 파라미터가 꼭 없어도 된다. 없으면 null
+    만약 true 였으면 무조건 있어야 하며, 없을 경우 에러남
+    URL에 name 요청값이 있으면 문자열로 받아서 name 변수에 넣고 없어도 에러 내지 말기
+    String name 은 받을 값의 자료형을 써준 것
+
+  */
+    //전체조회
+    @GetMapping
+    public ResponseEntity<List<GetScheduleResponse>> getAllSchedule(
             @RequestParam(required = false) String name
-            //String name 은 받을 값의 자료형을 써준 것
     ) {
         //리턴 값은 Service에서 가져온 dto 값 반환
         return ResponseEntity.status(HttpStatus.OK).body(scheduleService.getAll(name));
     }
 
     //선택 일정 조회
-    @GetMapping("/schedules/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<GetScheduleResponse> getOneSchedule(@PathVariable Long id) {
         return ResponseEntity.status(HttpStatus.OK).body(scheduleService.getOne(id));
     }
 
-    @PutMapping("/schedules/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<UpdateScheduleResponse> updateSchedule(@PathVariable Long id, @RequestBody UpdateScheduleRequest request) {
         return ResponseEntity.status(HttpStatus.OK).body(scheduleService.update(id, request.getScheduleName(), request.getName(), request.getPassword()));
     }
 
-    @DeleteMapping("/schedules/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteSchedule(@PathVariable Long id, @RequestBody DeleteScheduleRequest request) {
         scheduleService.delete(id,request.getPassword());
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
--- src/main/java/com/example/schedule_project/dto/UpdateScheduleRequest.java
+++ src/main/java/com/example/schedule_project/dto/UpdateScheduleRequest.java
@@ -5,8 +5,8 @@ import lombok.Getter;
 @Getter
 public class UpdateScheduleRequest {
 
-    private String scheduleName;
-    private String name;
-    private String password;
+    private  String scheduleName;
+    private  String name;
+    private  String password;
 
 }
